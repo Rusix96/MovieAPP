7864781eecd1f313bfb3ba2ae31f3e7f95631579
Rusix96 <rusix96@hotmail.com>
2019-06-12 01:32:01 +0200

    1- TableView / TableViewCell configuration 2 - Get petitions from Movie Database API, obtain the title and overview of movies.

---
+++ MovieAPP/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  MovieAPP
+//
+//  Created by Rusik on 12/06/2019.
+//  Copyright © 2019 Rusik. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+ 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+  
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
---
+++ MovieAPP/MovieViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  MovieAPP
+//
+//  Created by Rusik on 11/06/2019.
+//  Copyright © 2019 Rusik. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var movies: [NSDictionary]?
+  
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        fetchMovies()
+        
+    }
+    func fetchMovies() {
+    let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = URLRequest (
+            url: url!,
+            cachePolicy: URLRequest.CachePolicy.reloadIgnoringLocalCacheData,
+            timeoutInterval: 10)
+        
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate: nil,
+            delegateQueue: OperationQueue.main
+        )
+        
+        let task: URLSessionDataTask = session.dataTask(
+            with: request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        print("response: \(responseDictionary)")
+                        self.movies = responseDictionary["results"] as? [NSDictionary]
+                        self.tableView.reloadData()
+                        
+                    }
+                }
+        })
+        task.resume()
+        
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //If all OK, return all the movies, else, default 0
+        return movies?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for:  indexPath)  as! MovieCell
+        let movie = movies![indexPath.row]
+        
+        let overview = movie["overview"] as! String
+        let title = movie["title"] as! String
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        cell.textLabel?.text = title
+        
+        return cell
+    }
+
+}
+
